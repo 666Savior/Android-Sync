2bfd497f185af63b6396f28eb8ba80a0e0c610bf
Ryan <raw9426@gmail.com>
2021-10-11 03:57:45 -0400

    Add method for finding a filename in another directory

--- files.py
+++ files.py
@@ -28,10 +28,38 @@ def dirDiff(direcPrimary, direcSecondary):
         primaryFnames.append(os.path.split(file)[1])
     for file in secondaryFiles:
         secondaryFnames.append(os.path.split(file)[1])
-        
 
-    diffList = [f for f in primaryFnames if f not in secondaryFnames]
+
+    #diffList = [f for f in primaryFnames if f not in secondaryFnames]
+    diffList = [f for f in primaryFiles if os.path.split(f)[1] not in secondaryFnames]
+
+    for i in range(5):
+        print(diffList[i])
 
     filesLog.info("Total file difference is %d files" % len(diffList))
 
     return diffList
+
+def locateFile(direcStart, maxDepth, fname):
+    """Searches for the given filename starting in the given start directory and recursively scanning to the specified depth.
+    Returns None if the the cannot be found within the given parameters"""
+
+    fileList = []
+
+    for depth in range(1, maxDepth + 1):
+        if depth == 1:
+            maxGlob = fname
+        else:
+            maxGlob = ("*/" * (depth - 1) +fname)
+
+        filesLog.info(maxGlob)
+
+        searchDir = os.path.join(direcStart, maxGlob)
+        filesLog.info(searchDir)
+
+        allFiles = glob.glob(searchDir)
+        fileList.extend(file for file in allFiles if (os.path.split(file)[1] == fname and file != os.path.join(direcStart, fname)))
+
+        filesLog.info("Found filename in %d other location(s)" % len(fileList))
+        
+    return
